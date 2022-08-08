6891eb3c74b5d0369c796a416322b73f1f18a784
Stevecsob <steve.csobral@gmail.com>
2022-08-08 19:37:12 -0300

    criando primeiro @requestMapping

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,6 +1,7 @@
 package com.example.demo.student;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -8,6 +9,7 @@ import java.time.Month;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/student")
 public class StudentController {
     @GetMapping
     public List<Student> hello() {
